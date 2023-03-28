cfd90be54a0497da692a7b1a27eb40868c2156f1
Jayson Ambrose <121517689+jayson-ambrose@users.noreply.github.com>
2023-03-28 09:47:12 -0600

    Adding SQLAlchemy and Alembic stuff

--- encounter_counter.py
+++ encounter_counter.py
@@ -1,3 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from db.models import Truther, UFO, Sighting
+from helpers import create_table, create_sighting, create_truther, login
+
+import click
+# from lib.x_cli import tacos
+
+#connect with db
+engine = create_engine('sqlite:///encounter_counter.db')
+session = sessionmaker(bind=engine)
+
 import click
 from lib.helpers import main_menu, report
 
