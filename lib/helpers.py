57772049503f2712c82b30203e2d27e77ed499f7
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-29 12:41:29 -0600

    with error handling

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,4 +1,5 @@
 import click
+import re
 from lib.db.models import (Sighting, Truther, Base)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -16,6 +17,13 @@ def main_menu_text():
             [quit    (q)]  : Exit Encounter Counter.
         ''')
 
+def build_regex(pattern, string):
+
+    regex = re.compile(pattern)
+    match = regex.search(string)
+
+    return match
+
 def main_menu ():
 
     choice = ""
@@ -32,23 +40,42 @@ def main_menu ():
         choice = click.prompt("Enter selection")
 
 def check_location_valid(string):
-    #these functions will be used to verify form input so that it matches the rest of the DB to avoid errors.
-    pass
+    
+    pattern = r"^[A-z]{2,25},\s[A-Z]{2}$"
+    if string == None:
+        return False
+    elif build_regex(pattern, string) != None:
+        return True
+    else:
+        return False
 
 def check_time_valid(string):
-    pass
+
+    pattern = r"^(2[0-3]|[01][0-9]):([0-5][0-9])$"
+    if string == None:
+        return False
+    elif build_regex(pattern, string) != None:
+        return True
+    else:
+        return False
 
 def check_date_valid(string):
-    pass
+    #accepts a string as follows [any 4 digits]-[2 dig number between 1-12]-[2 dig number between 01 and 31]
+    pattern = r"^[0-9]{4}-(0[1-9]|1[0-2])-(3[0-1]|0[1-9]|[1-2][0-9])$"
+    if string == None:
+        return False
+    elif build_regex(pattern, string) != None:
+        return True
+    else:
+        return False
 
 def check_encounter_type_valid(string):
-    pass
-
-def check_summary_valid(string):
-    pass
-
-def check_ufo_shape_valid(string):
-    pass
+    if string == None:
+        return False
+    elif (string == "sighting") or (string == "greeting") or (string == "abduction"):
+        return True
+    else:
+        return False
 
 def current_user_check(username):
     current_user = session.query(Truther).filter(Truther.username == username)
@@ -58,6 +85,7 @@ def current_user_check(username):
     #return session.query(Truther).filter(Truther.username == username) 
 
 def report_sighting():
+    import ipdb
 
     click.echo("UFO OR UAP ENCOUNTER REPORT")
 
@@ -76,15 +104,23 @@ def report_sighting():
             input_summary = None
             input_ufo_shape = None
 
-            #while(check_location_valid(input_location) == False):
-            input_location = click.prompt("Where did the event occur? (City, ST)", type=str)
+            while(check_location_valid(input_location) == False):
+                input_location = click.prompt("Where did the event occur? (City, ST): ", type=str)
+            
+            while(check_time_valid(input_time) == False):
+                input_time = click.prompt("What time did the event occur? (24-hr clock time): ", type=str)
 
-            input_time = click.prompt("What time did the event occur? (24-hr clock time)", type=str)
-            input_date = click.prompt("What was the date? (YYYY-MM-DD)", type=str)
+            while(check_date_valid(input_date) == False):
+                input_date = click.prompt("What was the date? (YYYY-MM-DD)", type=str)
+
             input_duration = click.prompt("For how long did the event continue? (sec)", type=int)
-            input_encounter_type = click.prompt("What type of encounter did you experience? (sighting, greeting, abduction)", type=str)
+
+            while(check_encounter_type_valid(input_encounter_type) == False):
+                input_encounter_type = click.prompt("What type of encounter did you experience? (sighting, greeting, abduction)", type=str)
+
             input_summary = click.prompt("Please enter a brief description of the event", type=str)
-            input_ufo_shape = click.prompt("What shape was the object?", type=str)
+
+            input_ufo_shape = click.prompt("What shape was the object?: ", type=str)
 
             print(f'''
             ENTERED VALUES:
@@ -111,9 +147,10 @@ def report_sighting():
             session.add(event)
             session.commit()
 
-        choice = click.prompt("Report Encounter? (Y/N)")
-        print('Returning to main menu.')
-        main_menu_text()
+        choice = click.prompt("Report Encounter? (Y/N): ")
+
+    print('Returning to main menu.')
+    main_menu_text()
 
 def profile():
     click.echo("....................................")
