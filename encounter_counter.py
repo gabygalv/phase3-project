270b9f4c5ca314ddeda29e9e7083160cbc54071a
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-03-30 00:57:23 -0600

    beautified some menus. abstracted some logic for better readability and less nesting

--- encounter_counter.py
+++ encounter_counter.py
@@ -1,7 +1,18 @@
 import click
+from lib.helpers import report_sighting, search, main_menu_text
 
-from lib.db.models import Truther, UFO, Sighting
-from lib.helpers import main_menu, report_sighting
+def main_menu ():
+
+    choice = ""
+    main_menu_text()
+
+    while choice != 'quit' and choice != 'q' and choice != 'Q':
+        if (choice in ['r', 'R', 'report']):
+            report_sighting()
+        if (choice in ['s', 'S', 'search']):
+            search()
+
+        choice = click.prompt("Enter selection")
 
 def encounter_counter():
     main_menu()    
