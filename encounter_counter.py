2cae17e16acacb0e8372d65c0ae6227e7890fc2e
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-03-31 08:05:47 -0700

    exit sequence

--- encounter_counter.py
+++ encounter_counter.py
@@ -1,4 +1,5 @@
 import click
+import time
 from lib.helpers import report_sighting, search, main_menu_text
 
 def main_menu ():
@@ -16,6 +17,66 @@ def main_menu ():
 
 def encounter_counter():
     main_menu()    
-
-    print("Thank you for using Encounter Counter!")
-
+    click.echo("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+    click.echo("Thank you for using Encounter Counter!")
+    click.echo("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
+    time.sleep(1.5)
+    click.echo("ALERT!!!")
+    time.sleep(.5)
+    click.echo("Our servers are unstable, your base location may been compromi--")
+    time.sleep(1)
+    click.echo("""
+                _____
+             ,-"     "-.
+            / o       o \u005c
+           /   \     /   \u005c
+          /     )-"-(     \u005c
+         /     ( 6 6 )     \u005c
+        /       \ " /       \u005c
+       /         )=(         \u005c
+      /   o   .--"-"--.   o   \u005c
+     /    I  /  -   -  \  I    \u005c
+ .--(    (_}y/\       /\y{_)    )--.
+(    ".___l\/__\_____/__\/l___,"    )
+ \                                 /
+  "-._      o O o O o O o      _,-"
+      `--Y--.___________.--Y--'
+         |==.___________.==| 
+         `==.___________.==' 
+    """)
+    time.sleep(.5)
+    click.echo("Greetings, human! I am Zorax, your Uber driver for today")
+    time.sleep(.5)
+    click.echo("...")
+    time.sleep(1)
+    click.echo("What do you mean you didn't order an intergalacic Uber?!?")
+    time.sleep(.5)
+    click.echo("...")
+    time.sleep(.75)
+    click.echo("ahh, my navigation system is acting up again...")
+    time.sleep(1)
+    click.echo("...")
+    time.sleep(.5)
+    click.echo("Well.. nice to meet you. GTG!")
+    time.sleep(1)
+    click.echo("""
+     .              +   .                .   . .     .  .
+                   .                    .       .     *
+  .       *             . .           . . . .  .   .  + .
+            .     .       .   .    .   .  +  . . .
+.                 .              .  .   .    .    . .
+    .     .     . +.    +  .        .     .     . +.    +  .
+     .       .   . .     . .       .       .   . .
+        . .                 .    * . . .  .  +   .
+           +      .           .   .      +
+                            .       . +  .+. .
+  .                      .     . + .  . .     .      .
+           .      .    .     . .   . . .        ! /
+      *             .    . .  +    .  .       - O -
+          .     .    .  +   . .  *  .       . / |
+               . + .  .  .  .. +  .
+.      .  .  .  *   .  *  . +..  .            *
+ .      .   . .   .   .   . .  +   .    .            +
+    """)
+    time.sleep(.5)
+    click.echo("Type 'encounter' to restart Encounter Counter")
